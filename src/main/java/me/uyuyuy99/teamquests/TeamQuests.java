f4fb4ffb79aecb78d23028ce4ed9f8633325b00a
uyuyuy99 <uyuyuy99@gmail.com>
2025-06-29 11:23:24 -0700

    Initial commit

---
+++ src/main/java/me/uyuyuy99/teamquests/TeamQuests.java
@@ -0,0 +1,17 @@
+package me.uyuyuy99.teamquests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeamQuests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
